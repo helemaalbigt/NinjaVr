aebc76cf8fc400b0ecd7051398fcb1c9ecf8815e
ThomasVanBouwel <van.bouwel.thomas@gmail.com>
2019-05-11 16:16:13 -0400

    avatar setup

---
+++ Assets/#Project/Common/Scripts/CopyTransform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+    public Transform target;
+    public bool useLocal;
+
+    void Update()
+    {
+        if (useLocal)
+        {
+            transform.localPosition = target.localPosition;
+            transform.localRotation = target.localRotation;
+        }
+        else
+        {
+            transform.position = target.position;
+            transform.rotation = target.rotation;
+        }
+    }
+}
---
+++ Assets/#Project/Player/Scripts/AvatarView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarView : MonoBehaviour
+{
+    public UserStyleList _style;
+    public Renderer _head;
+    public Renderer _handL;
+    public Renderer _handR;
+
+    private AttackingPlayer _player;
+    public AttackingPlayer player
+    {
+        get { return _player; }
+        set
+        {
+            SetStyle(_style.GetStyle(value));
+            _player = value;
+        }
+    }
+
+    void Start()
+    {
+        player = AttackingPlayer.one;
+    }
+
+    public void SetStyle(AttackerStyle style)
+    {
+        _head.material.color = style.avatarColor;
+        _handL.material.color = style.avatarColor;
+        _handR.material.color = style.avatarColor;
+    }
+}
