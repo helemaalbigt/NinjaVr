a4401b26413a201c336414fc1de8c15393d6e21d
ThomasVanBouwel <van.bouwel.thomas@gmail.com>
2019-05-11 22:30:50 -0400

    simple rigged hands

---
+++ Assets/#Project/Player/Scripts/HandControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControl : MonoBehaviour
+{
+    public Animator _leftAnimator;
+    public Animator _rightAnimator;
+
+    void Update()
+    {
+        _leftAnimator.SetFloat("HandPos", OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger));
+        _rightAnimator.SetFloat("HandPos", OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger));
+    }
+}
