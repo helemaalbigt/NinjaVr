7af7aaa29fb2a7a51b2b3d1c4731fbbcb0681524
johncorn <jcornacchioli@gmail.com>
2019-05-16 23:12:26 -0400

    updated hand to animate for local players inputs only

--- Assets/#Project/Player/Scripts/HandControl.cs
+++ Assets/#Project/Player/Scripts/HandControl.cs
@@ -1,15 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Normal.Realtime;
 
-public class HandControl : MonoBehaviour
-{
+public class HandControl : RealtimeComponent {
+
     public Animator _leftAnimator;
     public Animator _rightAnimator;
 
-    void Update()
-    {
-        _leftAnimator.SetFloat("HandPos", OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger));
-        _rightAnimator.SetFloat("HandPos", OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger));
+    private RealtimeView _realtimeView;
+
+    private HandControlModel _model;
+    public HandControlModel model {
+        set { SetModel(value); }
+        get { return _model; }
+    }
+
+
+    private void Start() {
+        _realtimeView = GetComponent<RealtimeView>();
+    }
+
+    void Update() {
+        if (!_realtimeView.isOwnedLocally)
+            return;
+
+        _model.leftHandAnimationValue  = OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger);
+        _model.rightHandAnimationValue = OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger);
+    }
+
+    void SetModel(HandControlModel model) {
+        if (_model != null) {
+            // Clear events
+            _model.leftHandAnimationValueDidChange  -= LeftHandAnimationValueChanged;
+            _model.rightHandAnimationValueDidChange -= RightHandAnimationValueChanged;
+
+        }
+
+        _model = model;
+
+        if (_model != null) {
+            // Register for events
+            _model.leftHandAnimationValueDidChange  += LeftHandAnimationValueChanged;
+            _model.rightHandAnimationValueDidChange += RightHandAnimationValueChanged;
+        }
+    }
+
+    void LeftHandAnimationValueChanged(HandControlModel model, float value) {
+        _leftAnimator.SetFloat("HandPos", value);
+    }
+
+    void RightHandAnimationValueChanged(HandControlModel model, float value) {
+        _rightAnimator.SetFloat("HandPos", value);
     }
 }
