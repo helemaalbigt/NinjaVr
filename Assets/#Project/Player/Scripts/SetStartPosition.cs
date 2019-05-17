fa4c53867717d52b377ce83976db05e4aecb14e2
johncorn <jcornacchioli@gmail.com>
2019-05-16 21:43:09 -0400

    set position with camera offset

--- Assets/#Project/Player/Scripts/SetStartPosition.cs
+++ Assets/#Project/Player/Scripts/SetStartPosition.cs
@@ -6,6 +6,12 @@ using Normal.Realtime;
 public class SetStartPosition : MonoBehaviour {
 
     [SerializeField]
+    private Transform _playerHead;
+
+    [SerializeField]
+    private Transform _cameraRig;
+
+    [SerializeField]
     private Realtime _realtime;
 
     [SerializeField]
@@ -21,7 +27,15 @@ public class SetStartPosition : MonoBehaviour {
     void PlacePlayer(Realtime realtime) {
         Transform startingPoint = _realtime.clientID == 0 ? _player1StartingPoint : _player2StartingPoint;
 
-        transform.position = startingPoint.position;
-        transform.rotation = startingPoint.rotation;
+        Debug.Log("SetStartPosition PlacePlayer clientID " + _realtime.clientID + " " + startingPoint.name);
+
+
+        _cameraRig.position = startingPoint.position;
+        Vector3 temp = Vector3.ProjectOnPlane(startingPoint.position - _playerHead.position, Vector3.up);
+        _cameraRig.Translate(temp);
+        //_cameraRig.rotation = startingPoint.rotation;
+
+        //_playerHead.position = new Vector3(startingPoint.position.x, _playerHead.position.y, startingPoint.position.z);
+        //_playerHead.rotation = startingPoint.rotation;
     }
 }
